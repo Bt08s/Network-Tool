91169bc10527526236b6ac569ed2e17869a38280
Bt08s <68190921+Bt08s@users.noreply.github.com>
2023-11-20 13:16:31 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -6,9 +6,49 @@ import time
 
 dpg.create_context()
 
-with dpg.window(label="IP Scan", width=600, height=220, pos=(0, 211), no_resize=True, no_close=True, no_collapse=True, no_move=True, no_scrollbar=True):
+with dpg.window(label="Port scan", width=300, height=340, pos=(600, 0), no_resize=True, no_close=True, no_collapse=True, no_move=True, no_scrollbar=True):
+    def scan_port(ip, port):
+        global open_ports
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.settimeout(1)
+            sock.connect((ip, port))
+            sock.close()
+            open_ports.append(port)
+        except:
+            pass
+
+
+    def port_scanner():
+        global open_ports
+        open_ports = []
+
+        ip = dpg.get_value("port_scanner_ip")
+        start_port = int(dpg.get_value("port_scanner_start_port"))
+        end_port = int(dpg.get_value("port_scanner_end_port"))
+
+        print(f"[PORT SCAN] IP {ip} Port {start_port}:{end_port}")
+        threads = []
+        for port in range(start_port, end_port + 1):
+            thread = threading.Thread(target=scan_port, args=(ip, port))
+            threads.append(thread)
+            thread.start()
+
+        for thread in threads:
+            thread.join()
+
+        dpg.set_value("port_scan_output", "\n".join(map(str, open_ports)))
+
+    dpg.add_input_text(label="IP", tag="port_scanner_ip")
+    dpg.add_input_text(label="Start port", default_value=1, tag="port_scanner_start_port")
+    dpg.add_input_text(label="End port", default_value=65535, tag="port_scanner_end_port")
+    dpg.add_button(label="Scan", callback=port_scanner)
+    dpg.add_spacer(parent=10)
+    dpg.add_input_text(multiline=True, readonly=True, tag="port_scan_output", height=190)
+
+with dpg.window(label="IP scan", width=600, height=175, pos=(0, 165), no_resize=True, no_close=True, no_collapse=True, no_move=True, no_scrollbar=True):
     def scan_ip():
-        dpg.show_item("ip_scan_loading")
+        print("[IP SCAN] Range 192.168.1.1/24")
         ip_range = "192.168.1.1/24"
         arp = ARP(pdst=ip_range)
         ether = Ether(dst="ff:ff:ff:ff:ff:ff")
@@ -17,18 +57,17 @@ with dpg.window(label="IP Scan", width=600, height=220, pos=(0, 211), no_resize=
         ip_mac_pairs = [(received.psrc, received.hwsrc) for sent, received in result]
         output = "\n".join([f"{ip} - {mac}" for ip, mac in ip_mac_pairs])
         dpg.set_value("ip_output", output)
-        dpg.hide_item("ip_scan_loading")
 
     dpg.add_input_text(multiline=True, readonly=True, tag="ip_output")
     dpg.add_button(label="Scan", callback=scan_ip)
-    dpg.add_loading_indicator(show=False, tag="ip_scan_loading")
 
-with dpg.window(label="UDP", width=300, height=211, pos=(300, 0), no_resize=True, no_close=True, no_collapse=True, no_move=True, no_scrollbar=True):
-    def udp_send(server_address, message):
+with dpg.window(label="UDP", width=300, height=165, pos=(300, 0), no_resize=True, no_close=True, no_collapse=True, no_move=True, no_scrollbar=True):
+    def udp_send(address, data):
         try:
-            client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            client_socket.sendto(message.encode(), server_address)
-            client_socket.close()
+            client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            client.sendto(data.encode(), address)
+            client.close()
+            print(f"[UDP SOCKET CLIENT] IP {address} Data {data}")
         except Exception as e:
             print(e)
             pass
@@ -36,42 +75,37 @@ with dpg.window(label="UDP", width=300, height=211, pos=(300, 0), no_resize=True
     def udp_client():
         try:
             server_address = (dpg.get_value("udp_ip"), int(dpg.get_value("udp_port")))
-            message = dpg.get_value("udp_message")
+            data = dpg.get_value("udp_data")
             count = int(dpg.get_value("udp_msg_count"))
 
             threads = []
             for _ in range(count):
-                thread = threading.Thread(target=udp_send, args=(server_address, message))
+                thread = threading.Thread(target=udp_send, args=(server_address, data))
                 threads.append(thread)
                 thread.start()
 
             for thread in threads:
                 thread.join()
 
-            dpg.set_value("udp_true_result", "True")
-            time.sleep(1)
-            dpg.set_value("udp_true_result", "")
+            print("[UDP CLIENT LOG] Finished successfully")
         except Exception as e:
-            dpg.set_value("udp_false_result", "False")
-            time.sleep(1)
-            dpg.set_value("udp_false_result", "")
-            print(e)
+            print(f"[TCP CLIENT LOG] Error {e}")
 
-    dpg.add_input_text(label="Address", default_value="127.0.0.1", tag="udp_ip")
-    dpg.add_input_text(label="Port", default_value="65535", tag="udp_port")
-    dpg.add_input_text(label="Message", default_value="Test!", tag="udp_message")
+    dpg.add_input_text(label="Address", tag="udp_ip")
+    dpg.add_input_text(label="Port", tag="udp_port")
+    dpg.add_input_text(label="Data", tag="udp_data")
     dpg.add_input_text(label="Count", default_value="1", tag="udp_msg_count")
     dpg.add_button(label="Send", callback=udp_client)
-    dpg.add_text(tag="udp_true_result", color=(0, 128, 0))
-    dpg.add_text(tag="udp_false_result", color=(255, 0, 0))
+    dpg.add_text(tag="udp_result", color=(255, 165, 0))
 
-with dpg.window(label="TCP", width=300, height=211, no_collapse=True, no_move=True, no_close=True, no_resize=True, no_scrollbar=True):
-    def tcp_send(server_address, message):
+with dpg.window(label="TCP", width=300, height=165, no_collapse=True, no_move=True, no_close=True, no_resize=True, no_scrollbar=True):
+    def tcp_send(address, data):
         try:
-            client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            client_socket.connect(server_address)
-            client_socket.sendall(message.encode())
-            client_socket.close()
+            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client.connect(address)
+            client.sendall(data.encode())
+            client.close()
+            print(f"[TCP SOCKET CLIENT] IP {address} Data {data}")
         except Exception as e:
             print(e)
             pass
@@ -83,36 +117,29 @@ with dpg.window(label="TCP", width=300, height=211, no_collapse=True, no_move=Tr
         client_socket.connect(server_address)
 
         try:
-            message = dpg.get_value("tcp_message")
+            data = dpg.get_value("tcp_data")
             count = int(dpg.get_value("tcp_msg_count"))
 
             threads = []
             for _ in range(count):
-                thread = threading.Thread(target=tcp_send, args=(server_address, message))
+                thread = threading.Thread(target=tcp_send, args=(server_address, data))
                 threads.append(thread)
                 thread.start()
 
             for thread in threads:
                 thread.join()
 
-            dpg.set_value("tcp_true_result", "True")
-            time.sleep(1)
-            dpg.set_value("tcp_true_result", "")
+            print("[TCP CLIENT LOG] Finished successfully")
         except Exception as e:
-            dpg.set_value("tcp_false_result", "False")
-            time.sleep(1)
-            dpg.set_value("tcp_false_result", "")
-            print(e)
-
+            print(f"[TCP CLIENT LOG] Error {e}")
         client_socket.close()
 
-    dpg.add_input_text(label="Address", default_value="google.com", tag="tcp_url")
-    dpg.add_input_text(label="Port", default_value="80", tag="tcp_port")
-    dpg.add_input_text(label="Message", default_value="Hello, server!", tag="tcp_message")
+    dpg.add_input_text(label="Address", tag="tcp_url")
+    dpg.add_input_text(label="Port", tag="tcp_port")
+    dpg.add_input_text(label="Data", tag="tcp_data")
     dpg.add_input_text(label="Count", default_value="1", tag="tcp_msg_count")
     dpg.add_button(label="Send", callback=tcp_client)
-    dpg.add_text(tag="tcp_true_result", color=(0, 128, 0))
-    dpg.add_text(tag="tcp_false_result", color=(255, 0, 0))
+    dpg.add_text(tag="tcp_result", color=(255, 165, 0))
 
 with dpg.theme() as global_theme:
     with dpg.theme_component(dpg.mvAll):
@@ -127,7 +154,7 @@ with dpg.theme() as global_theme:
         dpg.add_theme_style(dpg.mvStyleVar_ItemSpacing, 5, 5)
 
 dpg.bind_theme(global_theme)
-dpg.create_viewport(title='Network tool by Bt08s', width=616, height=470)
+dpg.create_viewport(title='Network tool by Bt08s', width=916, height=379)
 dpg.setup_dearpygui()
 dpg.show_viewport()
 dpg.start_dearpygui()
